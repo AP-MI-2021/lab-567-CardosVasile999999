5dbc2c2cd836e7a198d81378058614aa9f2a4f28
Cardos Vasile <cardosvasile15@gmail.com>
2021-10-26 14:39:33 +0200

    lab 5

--- Domain/rezervari.py
+++ Domain/rezervari.py
@@ -15,3 +15,57 @@ def gestioneaza_rezervari(id_rezervare, nume, clasa, pret, checkin):
         'pret': pret,
         'checkin': checkin
     }
+
+
+def get_id(rezervare):
+    """
+    Gett-er pentru id-ul rezervarii
+    :param rezervare: rezervarea
+    :return: id-ul rezervarii date ca parametru
+    """
+    return rezervare['id']
+
+
+def get_nume(rezervare):
+    """
+    Gett-er pentru numele rezervarii
+    :param rezervare: rezervarea
+    :return: numele rezervarii date ca parametru
+    """
+    return rezervare['nume']
+
+
+def get_clasa(rezervare):
+    """
+    Gett-er pentru clasa rezervarii
+    :param rezervare: rezervarea
+    :return: clasa rezervarii date ca parametru
+    """
+    return rezervare['clasa']
+
+
+def get_pret(rezervare):
+    """
+    Gett-er pentru pretul rezervarii
+    :param rezervare: rezervarea
+    :return: pretul rezervarii date ca parametru
+    """
+    return rezervare['pret']
+
+
+def get_checkin(rezervare):
+    """
+    Gett-er pentru checkin rezervarii
+    :param rezervare: rezervarea
+    :return: checkin-ul rezervarii date ca parametru
+    """
+    return rezervare['checkin']
+
+
+def get_str(rezervare):
+    """
+    Gett-er pentru id-ul rezervarii
+    :param rezervare: rezervarea
+    :return: id-ul rezervarii date ca parametru
+    """
+    return f'serbus {get_id(rezervare)}'
